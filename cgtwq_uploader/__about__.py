6039a8566082b5fb8a6d701c8fa7c13cbdad2d3a
NateScarlet <NateScarlet@Gmail.com>
2023-06-29 11:37:51 +0800

    fix: should use relative import

--- cgtwq_uploader/__about__.py
+++ cgtwq_uploader/__about__.py
@@ -1,5 +1,8 @@
 # -*- coding=UTF-8 -*-
+# pyright: strict, reportTypeCommentUsage=none
 """CGTeamwork batch uploader.   """
 
-from __version__ import VERSION as __version__
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .__version__ import VERSION as __version__
 __author__ = 'NateScarlet@Gmail.com'
